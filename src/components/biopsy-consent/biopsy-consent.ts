588a91a91a6a87d0c25d7f239e9cde50d5ddb736
Ghulam Asghar <ghulamasghar7678@gmail.com>
2025-09-09 17:24:28 +0500

    Add responsive biopsy consent form with signature pad
    Introduces a responsive biopsy consent form with patient name binding, date input, and a signature pad using signature_pad. Adds PDF export functionality via html2canvas and jsPDF. Refactors SCSS/CSS for mobile and tablet responsiveness, adds mixins for media queries, and updates Angular template and component logic accordingly. Removes external Font Awesome CSS and comments out unused components in app.html.

--- src/components/biopsy-consent/biopsy-consent.ts
+++ src/components/biopsy-consent/biopsy-consent.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, signal, ViewChild } from '@angular/core';
+import SignaturePad from 'signature_pad';
+import { FormsModule } from '@angular/forms';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-biopsy-consent',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './biopsy-consent.html',
   styleUrl: './biopsy-consent.css'
 })
-export class BiopsyConsent {
+export class BiopsyConsent implements OnInit, AfterViewInit {
+  @ViewChild("biopsyPatientSignature", { static: true }) canvas!: ElementRef;
+  @ViewChild("biopsy", { static: true }) biopsy!: ElementRef;
+  sig!: SignaturePad;
+  patName = signal("");
+  date = signal("");
+  ngOnInit(): void { }
+
+  ngAfterViewInit() {
+    this.resizeCanvas();
+    this.sig = new SignaturePad(this.canvas.nativeElement)
+  }
+
+  resizeCanvas() {
+    const canvasEl = this.canvas.nativeElement;
+    const ratio = Math.max(window.devicePixelRatio || 1, 1);
+    canvasEl.width = canvasEl.offsetWidth * ratio;
+    canvasEl.height = canvasEl.offsetHeight * ratio;
+    canvasEl.getContext("2d")!.scale(ratio, ratio);
+  }
+
+  clearSignature() {
+    this.sig.clear();
+  }
+
+  undoSignature() {
+    const result = this.sig.toData();
+    if (result.length) {
+      result.pop();
+      this.sig.fromData(result)
+    }
+  }
+
+  onSubmit() {
+    this.generatePDF();
+  }
+
+  generatePDF() {
+    const element = this.biopsy.nativeElement;
+    if (element) {
+      html2canvas(element).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        const imgProps = pdf.getImageProperties(imgData);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        pdf.save('download.pdf');
+      });
+    }
+  }
+
 
 }
